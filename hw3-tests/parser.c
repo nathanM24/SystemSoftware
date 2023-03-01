afa56e888f56179dc0ae3c694b9c0c7123f168af
nathanM24 <90732881+nathanM24@users.noreply.github.com>
2023-03-01 10:31:38 -0500

    file
    open file

--- hw3-tests/parser.c
+++ hw3-tests/parser.c
@@ -5,5 +5,12 @@ void parser_open();
 
 int main(int argc, char *argv[])
 {
+	FILE *file;
+    if(argc != 2)
+    {
+        printf("File is Needed!\n");
+        exit(1);
+    }
+    file = fopen(argv[1], "r");
 	return 0;
 }
